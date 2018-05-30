9183c0c626cb8b90fa70b90ad9c4d7eff6981667
Lachezar Kostov <lachezarkostov@gmail.com>
2018-05-30 13:33:42 +0100

    Add project files.

---
+++ GeoJSON/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
